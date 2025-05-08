2be792e856ebd229bf1ebadd057255ee9d86bd78
Ayoosh <>
2025-05-08 10:03:02 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+let btnName = document.getElementById("btnName")
+let btnFontSizeMore = document.getElementById("btnFontSizeMore")
+let btnFontSize_less = document.getElementById("btnFontSize_less")
+let btnColor = document.getElementById("btnColor")
+let btnBackgroundcolor = document.getElementById("btnBackgroundcolor")
+let text = document.getElementById("text")
+
+btnName.addEventListener("click", () =>{
+    text.innerText= "welcome"
+});
+
+btnFontSizeMore.addEventListener("click", () =>{
+    text.style.fontSize = "5rem"
+});
+
+btnFontSize_less.addEventListener("click", () =>{
+    text.style.fontSize = "10px"
+});
